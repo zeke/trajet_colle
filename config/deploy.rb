fdfef767b6944f549054cc28539680527e7847fc
Zeke Sikelianos <zeke@sikelianos.com>
2009-04-06 14:33:14 -0700

    Added initial non-working cap deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,14 +2,12 @@ set :application, "trajet_colle"
 set :scm, :git
 set :repository,  "git@github.com:zeke/trajet_colle.git"
 set :user, "root"
-# set :scm_passphrase, "secret"
 set :deploy_to, "/var/www/#{application}"
 set :use_sudo, false
 set :branch, "master"
 set :deploy_via, :remote_cache
 
 ssh_options[:forward_agent] = true
-default_run_options[:pty] = true
 
 role :app, "209.20.73.226"
 role :web, "209.20.73.226"
@@ -19,9 +17,6 @@ namespace :deploy do
   desc "Restarting passenger with restart.txt"
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "chmod 0777 #{current_path}/tmp"
-    # run "mkdir -p #{current_path}/log"
-    # run "touch #{current_path}/log/production.log"
-    # run "chmod 0666 #{current_path}/log/production.log"
     run "export LD_LIBRARY_PATH=/usr/local/lib"
 
     %w{files}.each do |share|
